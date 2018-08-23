bb8ea1c77c11c53d5bfe4c281a8ab36815e47331
Dave Longley <dlongley@digitalbazaar.com>
2018-08-23 15:36:37 -0400

    Add JSON.parse loop benchmark.

---
+++ index5.js
@@ -0,0 +1,44 @@
+const Benchmark = require('benchmark');
+
+const suite = new Benchmark.Suite();
+
+const opJsons = require('./arrayOfJson');
+
+function loop1() {
+  return opJsons.map(JSON.parse);
+}
+
+function loop2() {
+  const list = [];
+  for(const json of opJsons) {
+    list.push(JSON.parse(json));
+  }
+  return list;
+}
+
+function loop3() {
+  return JSON.parse('[' + opJsons.join(',') + ']');
+}
+
+function loop4() {
+  return JSON.parse(`[${opJsons.join()}]`);
+}
+
+const justParse = `[${opJsons.join()}]`;
+function loop5() {
+  return JSON.parse(justParse);
+}
+
+suite
+  .add('.map(JSON.parse) loop', () => loop1())
+  .add('for of JSON.parse loop', () => loop2())
+  .add('JSON.parse once string concat', () => loop3())
+  .add('JSON.parse once template string', () => loop4())
+  .add('JSON.parse once just parse', () => loop5())
+  .on('cycle', event => {
+    console.log(String(event.target));
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  .run();
