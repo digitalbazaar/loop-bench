1c6ece4e22370348ff8491ee15a6d0f9f0f7b05d
Matthew Collier <mcollier@digitalbazaar.com>
2019-10-22 13:47:38 -0400

    Add index21 for testing hasValue.

---
+++ index21.js
@@ -0,0 +1,34 @@
+const Benchmark = require('benchmark');
+
+const suite = new Benchmark.Suite();
+
+function loop1(obj, key, value) {
+  return [].concat(obj[key]).includes(value);
+}
+function loop2(obj, key, value) {
+  if(Array.isArray(obj[key])) {
+    return obj[key].includes(value);
+  }
+  return obj[key] === value;
+}
+
+const testObjAlpha = {type: 'Foo'};
+const testObjBeta = {type: ['Foo']};
+const testObjGamma = {type: ['Foo', 'Bar', 'Cat']};
+
+suite
+  .add('concat - string', () => loop1(testObjAlpha, 'type', 'Foo'))
+  .add('concat - one element array', () => loop1(testObjBeta, 'type', 'Foo'))
+  .add('concat - one element array', () => loop1(testObjGamma, 'type', 'Foo'))
+
+  .add('test - string', () => loop2(testObjAlpha, 'type', 'Foo'))
+  .add('test - one element array', () => loop2(testObjBeta, 'type', 'Foo'))
+  .add('test - one element array', () => loop2(testObjGamma, 'type', 'Foo'))
+
+  .on('cycle', event => {
+    console.log(String(event.target));
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  .run();
